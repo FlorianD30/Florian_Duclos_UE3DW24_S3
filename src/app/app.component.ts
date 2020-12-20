60b9982eed81b415a6472091be35a1982c3a95ab
Florian Duclos <florianduclos30@gmail.com>
2020-12-20 04:08:04 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Florian-Duclos-UE3DW24-S3';
+}
